5aa434e2544b1a7c8d5fa518b553b0dd8dbd7557
Alaa <aayyaser@hotmail.com>
2024-09-11 17:42:52 +0300

    fix warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import Home from "./views/e-commerce/pages/Home";
-import ProductDetails from "./views/e-commerce/pages/ProductDetails";
-import Dashboard from "./views/dashboard/pages/Dashboard";
-import Products from "./views/dashboard/pages/Products";
+
 
 function App() {
   return (
