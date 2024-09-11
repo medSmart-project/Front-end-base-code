efd68aa6c4cee444518bfb85d0d412efd8f09725
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2024-09-11 12:53:20 +0300

    add changes

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -5,18 +5,18 @@ import { FaStar } from "react-icons/fa6";
 function Review(props) {
   return (
     <>
-      <div class="container mx-auto flex items-start justify-between border-b-2 border-gray-300 p-6 my-3 relative w-3/4">
-        <div class="flex flex-col items-center">
-          <img src={user} alt="" class="w-24 h-24 rounded-full mb-2" />
-          <p class="text-sm font-semibold m-2">{props.name}</p>
+      <div className="container mx-auto flex items-start justify-between border-b-2 border-gray-300 p-6 my-3 relative w-3/4">
+        <div className="flex flex-col items-center">
+          <img src={user} alt="" className="w-24 h-24 rounded-full mb-2" />
+          <p className="text-sm font-semibold m-2">{props.name}</p>
         </div>
 
-        <div class="flex-1 ml-5">
-          <p class="text-xs text-gray-500 absolute top-1 right-6">
+        <div className="flex-1 ml-5">
+          <p className="text-xs text-gray-500 absolute top-1 right-6">
             Reviewed on Sep 7, 2024
           </p>
 
-          <p class="text-sm m-2 text-secondary">
+          <p className="text-sm m-2 text-secondary">
             This product fixes my hair very deeply, amazing and very
             recommended.
           </p>
